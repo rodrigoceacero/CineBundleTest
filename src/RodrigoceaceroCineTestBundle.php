eae75a4c19938a2c0781147b1b67072bcfd1179f
Rodrigo Ceacero <rceaare426@g.educaand.es>
2024-04-10 10:46:31 +0000

    Correcion del bundle.php

--- src/RodrigoceaceroCineTestBundle.php
+++ src/RodrigoceaceroCineTestBundle.php
@@ -2,9 +2,9 @@
 
 namespace Rodrigoceacero\CineTestBundle;
 
-use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class RodrigoceaceroCineTestBundle extends AbstractBundle
+class RodrigoceaceroCineTestBundle extends Bundle
 {
     public function getPath(): string
     {
